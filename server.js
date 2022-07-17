6f6d58b4191f23b19c4a3887d5fef1ed572a3e11
Danil Pyatnitsev <danil@pyatnitsev.ru>
2022-07-17 14:12:05 +0300

    add random number

--- server.js
+++ server.js
@@ -6,10 +6,16 @@ const express = require('express');
 const port = 8080;
 const host = '0.0.0.0';
 
+function between(min, max) {
+    return Math.floor(
+        Math.random() * (max - min) + min
+    )
+}
+
 // приложение
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World! - ' + between(1,50));
 });
 
 app.listen(port, host);
